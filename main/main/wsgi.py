1edc2d39cc0792535676930e3ed8a32b7ee795d5
advait2711 <137433670+advait2711@users.noreply.github.com>
2024-10-26 23:57:16 +0530

    Update wsgi.py

--- main/main/wsgi.py
+++ main/main/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+# Add the project directory to the PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.main.settings')
 
 application = get_wsgi_application()
